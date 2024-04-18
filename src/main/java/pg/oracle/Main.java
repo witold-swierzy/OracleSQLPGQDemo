0b1dfec9a7f8ca1a1ab6043f4cdf9cc8d2470023
witold-swierzy <131450690+witold-swierzy@users.noreply.github.com>
2024-04-18 17:54:53 +0200

    Initial commit

---
+++ src/main/java/pg/oracle/Main.java
@@ -0,0 +1,67 @@
+package pg.oracle;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    static String PQ_EXECUTION_MODE;
+    static String PQ_PGX_URL;
+    static String PQ_USERNAME;
+    static String PQ_PASSWORD;
+    static String PQ_QUERY;
+    static int PQ_EXECUTIONS;
+    static String PQ_JDBC_URL;
+    static String PQ_TOKEN;
+
+    public static void initDemo() {
+        PQ_EXECUTION_MODE = System.getenv("PQ_EXECUTION_MODE").replace("\"","");
+        //PQ_EXECUTION_MODE = "PQ_PGX_MODE";
+        //PQ_EXECUTION_MODE = "PQ_REST_MODE";
+        //PQ_EXECUTION_MODE = "PQ_DB_MODE";
+        PQ_USERNAME       = System.getenv("PQ_USERNAME").replace("\"","");
+        PQ_PASSWORD       = System.getenv("PQ_PASSWORD").replace("\"","");
+        PQ_QUERY          = System.getenv("PQ_QUERY").replace("\"","");
+        PQ_EXECUTIONS     = Integer.valueOf(System.getenv("PQ_EXECUTIONS").replace("\"",""));
+        //PQ_EXECUTIONS     = 100;
+        if ( PQ_EXECUTION_MODE.equals("PQ_DB_MODE"))
+            PQ_JDBC_URL       = System.getenv("PQ_JDBC_URL").replace("\"","");
+        else
+            PQ_JDBC_URL       = "N/A";
+        if ( PQ_EXECUTION_MODE.equals("PQ_PGX_MODE") || PQ_EXECUTION_MODE.equals("PQ_REST_MODE"))
+            PQ_PGX_URL        = System.getenv("PQ_PGX_URL").replace("\"","");
+        else
+            PQ_PGX_URL        = "N/A";
+    }
+
+    public static void dbMode() {}
+
+    public static void RESTMode() {}
+
+    public static void PGXMode() {}
+
+    public static void main(String[] args) {
+        initDemo();
+        System.out.println("Execution mode       : "+PQ_EXECUTION_MODE);
+        System.out.println("PGX Server URL       : "+PQ_PGX_URL);
+        System.out.println("JDBC URL             : "+PQ_JDBC_URL);
+        System.out.println("Query                : "+PQ_QUERY);
+        System.out.println("Number of executions : "+PQ_EXECUTIONS);
+        System.out.println("Starting tests");
+        long start = System.currentTimeMillis();
+        if ( PQ_EXECUTION_MODE.equals("PQ_DB_MODE"))
+            dbMode();
+        if ( PQ_EXECUTION_MODE.equals("PQ_REST_MODE"))
+            RESTMode();
+        if ( PQ_EXECUTION_MODE.equals("PQ_PGX_MODE"))
+            PGXMode();
+        long end = System.currentTimeMillis();
+        System.out.println("Summary : ");
+        System.out.println("Execution mode           : "+PQ_EXECUTION_MODE);
+        System.out.println("PGX Server URL           : "+PQ_PGX_URL);
+        System.out.println("JDBC URL                 : "+PQ_JDBC_URL);
+        System.out.println("Query                    : "+PQ_QUERY);
+        System.out.println("Number of executions     : "+PQ_EXECUTIONS);
+        System.out.println("Total elsapsed time (ms) : "+(end-start));
+
+
+    }
+}
